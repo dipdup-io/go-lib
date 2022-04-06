4ebcffb39be9ef92f671183bfb5e07997c82da9b
Artem <aopoltorzhicky@gmail.com>
2022-04-06 15:19:07 +0300

    Add clickhouse to config validations

--- config/consts.go
+++ config/consts.go
@@ -2,7 +2,8 @@ package config
 
 // Supported database kinds
 const (
-	DBKindSqlite   = "sqlite"
-	DBKindPostgres = "postgres"
-	DBKindMysql    = "mysql"
+	DBKindSqlite     = "sqlite"
+	DBKindPostgres   = "postgres"
+	DBKindMysql      = "mysql"
+	DBKindClickhouse = "clickhouse"
 )
