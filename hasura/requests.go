2648663e52a7044d4a8f863a590cb9baea263cef
852Kerfunkle <852Kerfunkle@users.noreply.github.com>
2022-04-25 20:56:34 +0200

    Make DatabaseUrl interface{} again

--- hasura/requests.go
+++ hasura/requests.go
@@ -45,32 +45,7 @@ type Configuration struct {
 type ConnectionInfo struct {
 	UsePreparedStatements bool        `json:"use_prepared_statements"`
 	IsolationLevel        string      `json:"isolation_level"`
-	DatabaseUrl           DatabaseUrl `json:"database_url,omitempty"`
-}
-
-// DatabaseUrl -
-type DatabaseUrl string
-
-// DatabaseUrlFromEnv -
-type DatabaseUrlFromEnv struct {
-	FromEnv string `json:"from_env"`
-}
-
-// UnmarshalJSON -
-func (d *DatabaseUrl) UnmarshalJSON(data []byte) error {
-	var s string
-	if err := json.Unmarshal(data, &s); err == nil {
-		*d = DatabaseUrl(s)
-		return nil
-	}
-
-	var fromEnv DatabaseUrlFromEnv
-	if err := json.Unmarshal(data, &fromEnv); err != nil {
-		return err
-	}
-
-	*d = DatabaseUrl(fromEnv.FromEnv)
-	return nil
+	DatabaseUrl           interface{} `json:"database_url"`
 }
 
 // Source -
