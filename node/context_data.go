2d65136987c92963a83138036afb13e003d9aeef
Artem <aopoltorzhicky@gmail.com>
2022-06-06 16:31:10 +0300

    Node RPC: add rollup API

--- node/context_data.go
+++ node/context_data.go
@@ -112,3 +112,62 @@ const (
 	InactiveDelegateType DelegateType = "inactive"
 	AllDelegateType      DelegateType = "all"
 )
+
+// TxRollupState -
+type TxRollupState struct {
+	LastRemovedCommitmentHashes *LastRemovedCommitmentHashes `json:"last_removed_commitment_hashes,omitempty"`
+	FinalizedCommitments        RollupStateCommitment        `json:"finalized_commitments"`
+	UnfinalizedCommitments      RollupStateCommitment        `json:"unfinalized_commitments"`
+	UncommittedInboxes          RollupStateCommitment        `json:"uncommitted_inboxes"`
+	CommitmentNewestHash        *string                      `json:"commitment_newest_hash,omitempty"`
+	TezosHeadLevel              *uint64                      `json:"tezos_head_level,omitempty"`
+	BurnPerByte                 stdJSON.Number               `json:"burn_per_byte,omitempty"`
+	AllocatedStorage            stdJSON.Number               `json:"allocated_storage"`
+	OccupiedStorage             stdJSON.Number               `json:"occupied_storage"`
+	InboxEma                    uint64                       `json:"inbox_ema"`
+	CommitmentsWatermark        *uint64                      `json:"commitments_watermark,omitempty"`
+}
+
+// LastRemovedCommitmentHashes -
+type LastRemovedCommitmentHashes struct {
+	LastMessageHash string `json:"last_message_hash"`
+	CommitmentHash  string `json:"commitment_hash"`
+}
+
+// RollupStateCommitment -
+type RollupStateCommitment struct {
+	Next   *uint64 `json:"next,omitempty"`
+	Newest *uint64 `json:"newest,omitempty"`
+	Oldest *uint64 `json:"oldest,omitempty"`
+}
+
+// RollupCommitmentForBlock -
+type RollupCommitmentForBlock struct {
+	Commitment     RollupCommitment `json:"commitment"`
+	CommitmentHash string           `json:"commitment_hash"`
+	Committer      string           `json:"committer"`
+	SubmittedAt    uint64           `json:"submitted_at"`
+	FinalizedAt    uint64           `json:"finalized_at"`
+}
+
+// RollupCommitment -
+type RollupCommitment struct {
+	Level           uint64                   `json:"level"`
+	Messages        RollupCommitmentMessages `json:"messages"`
+	Predecessor     *string                  `json:"predecessor,omitempty"`
+	InboxMerkleRoot string                   `json:"inbox_merkle_root"`
+}
+
+// RollupCommitmentMessages -
+type RollupCommitmentMessages struct {
+	Count                 uint64 `json:"count"`
+	Root                  string `json:"root"`
+	LastMessageResultHash string `json:"last_message_result_hash"`
+}
+
+// TxRollupInbox -
+type TxRollupInbox struct {
+	InboxLength   uint64 `json:"inbox_length"`
+	CumulatedSize uint64 `json:"cumulated_size"`
+	MerkleRoot    string `json:"merkle_root"`
+}
