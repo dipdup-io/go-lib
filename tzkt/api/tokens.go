84a0fd4806df0d76716c985c865dbb27e73ce877
Artem <aopoltorzhicky@gmail.com>
2022-07-28 17:52:49 +0300

    Get tokens TzKT API

--- tzkt/api/tokens.go
+++ tzkt/api/tokens.go
@@ -11,3 +11,9 @@ func (tzkt *API) GetTokenTransfers(ctx context.Context, filters map[string]strin
 	err = tzkt.json(ctx, "/v1/tokens/transfers", filters, false, &transfers)
 	return
 }
+
+// GetTokens -
+func (tzkt *API) GetTokens(ctx context.Context, filters map[string]string) (tokens []data.Token, err error) {
+	err = tzkt.json(ctx, "/v1/tokens", filters, false, &tokens)
+	return
+}
