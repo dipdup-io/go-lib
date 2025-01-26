d4233269b73cbc6e73f35d521e4aa56c3df9caf0
pinkflower32 <enigma.schemes.61@icloud.com>
2025-01-26 22:24:19 +0100

    Typo fix hasura_integration_helpers.go

--- hasura/hasura_integration_helpers.go
+++ hasura/hasura_integration_helpers.go
@@ -27,7 +27,7 @@ func TestExpectedMetadataWithActual(
 ) {
 	var cfg config.Config
 	if err := config.Parse(configPath, &cfg); err != nil {
-		t.Fatalf("Error with reading configuratoin file: %s", err)
+		t.Fatalf("Error with reading configuration file: %s", err)
 	}
 
 	api := New(cfg.Hasura.URL, cfg.Hasura.Secret)
