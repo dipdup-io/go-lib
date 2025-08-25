f434b67ffdbbbf839dab81b053ed2e413b3d9403
Konst <johnysalvesen@gmail.com>
2025-08-25 16:52:13 +0300

    TzKT v1.16.0: added fields to DoubleBaking

--- tzkt/data/operations.go
+++ tzkt/data/operations.go
@@ -502,16 +502,22 @@ type Migration struct {
 
 // DoubleBaking -
 type DoubleBaking struct {
-	Type         string    `json:"type"`
-	ID           uint64    `json:"id"`
-	Level        uint64    `json:"level"`
-	Timestamp    time.Time `json:"timestamp"`
-	Block        string    `json:"block"`
-	Hash         string    `json:"hash"`
-	AccusedLevel uint64    `json:"accusedLevel"`
-	Accuser      *Address  `json:"accuser"`
-	Offender     *Address  `json:"offender"`
-	Quote        *Quote    `json:"quote,omitempty"`
+	Type                 string    `json:"type"`
+	ID                   uint64    `json:"id"`
+	Level                uint64    `json:"level"`
+	Timestamp            time.Time `json:"timestamp"`
+	Block                string    `json:"block"`
+	Hash                 string    `json:"hash"`
+	AccusedLevel         uint64    `json:"accusedLevel"`
+	SlashedLevel         uint64    `json:"slashedLevel"`
+	Accuser              *Address  `json:"accuser"`
+	Reward               uint64    `json:"reward"`
+	Offender             *Address  `json:"offender"`
+	LostStaked           uint64    `json:"lostStaked"`
+	LostUnstaked         uint64    `json:"lostUnstaked"`
+	LostExternalStaked   uint64    `json:"lostExternalStaked"`
+	LostExternalUnstaked uint64    `json:"lostExternalUnstaked"`
+	Quote                *Quote    `json:"quote,omitempty"`
 }
 
 // DoubleConsensus -
